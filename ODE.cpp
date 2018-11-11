c8b428b6af242215449a50230007ab596c23fce2
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-11-11 13:24:18 -0500

    progreso inicial

---
+++ ODE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <array>
+
+using namespace std;
+
+//##############PUNTO 1#################
+#define N 10000
+#define g 10.0
+#define c 0.2
+#define m 0.2
+
+
+float y_prima(float t, float x, float v);
+float y_prima_prima(float t, float x, float v);
+
+int main(){
+}
