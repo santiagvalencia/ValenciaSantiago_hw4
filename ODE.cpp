81ba669bf439a4ee28517024b8f95dda222d8586
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-11-11 20:51:41 -0500

    Runge-Kutta completo para el primer caso

--- ODE.cpp
+++ ODE.cpp
@@ -35,26 +35,64 @@ int main(){
   vy[0] = 300*sin(theta);
   t[0] = 0;
 
-  double k1_prima_x;
-  double k1_prima_y;
-  double k1_prima_vx;
-  double k1_prima vy;
+  double k1_prima_x; double k1_prima_y; double k1_prima_vx; double k1_prima_vy;
+  double k2_prima_x; double k2_prima_y; double k2_prima_vx; double k2_prima_vy;
+  double k3_prima_x; double k3_prima_y; double k3_prima_vx; double k3_prima_vy;
+  double k4_prima_x; double k4_prima_y; double k4_prima_vx; double k4_prima_vy;
+
+  double x_1; double y_1; double vx_1; double vy_1;
+  double x_2; double y_2; double vx_2; double vy_2;
+  double x_3; double y_3; double vx_3; double vy_3;
+
+  double k_prom_x; double k_prom_y; double k_prom_vx; double k_prom_vy;
 
 for(int i=1; i<N; i++){
-  k1_prima_x = dx_dt(x[i-1], vx[i-1]);
-  k1_prima_y = dy_dt(y[i-1], vy[i-1]);
-  k1_prima_vx = dvx_dt()
+  k1_prima_x = dx_dt(vx[i-1]);
+  k1_prima_y = dy_dt(vy[i-1]);
+  k1_prima_vx = dvx_dt(vx[i-1], vy[i-1]);
+  k1_prima_vy = dvy_dt(vx[i-1], vy[i-1]);
 
+  x_1 = x[i-1] + (dt/2)*k1_prima_x;
+  y_1 = y[i-1] + (dt/2)*k1_prima_y;
+  vx_1 = vx[i-1] + (dt/2)*k1_prima_vx;
+  vy_1 = vy[i-1] + (dt/2)*k1_prima_vy;
+  k2_prima_x = dx_dt(vx_1);
+  k2_prima_y = dy_dt(vy_1);
+  k2_prima_vx = dvx_dt(vx_1, vy_1);
+  k2_prima_vy = dvy_dt(vx_1, vy_1);
 
+  x_2 = x[i-1] + (dt/2)*k2_prima_x;
+  y_2 = y[i-1] + (dt/2)*k2_prima_y;
+  vx_2 = vx[i-1] + (dt/2)*k2_prima_vx;
+  vy_2 = vy[i-1] + (dt/2)*k2_prima_vy;
+  k3_prima_x = dx_dt(vx_2);
+  k3_prima_y = dy_dt(vy_2);
+  k3_prima_vx = dvx_dt(vx_2, vy_2);
+  k3_prima_vy = dvy_dt(vx_2, vy_2);
 
+  x_3 = x[i-1] + dt*k2_prima_x;
+  y_3 = y[i-1] + dt*k2_prima_y;
+  vx_3 = vx[i-1] + dt*k2_prima_vx;
+  vy_3 = vy[i-1] + dt*k2_prima_vy;
+  k4_prima_x = dx_dt(vx_3);
+  k4_prima_y = dy_dt(vy_3);
+  k4_prima_vx = dvx_dt(vx_3, vy_3);
+  k4_prima_vy = dvy_dt(vx_3, vy_3);
 
+  k_prom_x = (k1_prima_x + 2*k2_prima_x + 2*k3_prima_x + k4_prima_x);
+  k_prom_y = (k1_prima_y + 2*k2_prima_y + 2*k3_prima_y + k4_prima_y);
+  k_prom_vx = (k1_prima_vx + 2*k2_prima_vx + 2*k3_prima_vx + k4_prima_vx);
+  k_prom_vy = (k1_prima_vy + 2*k2_prima_vy + 2*k3_prima_vy + k4_prima_vy);
 
+  t[i] = t[i-1] + dt;
+  x[i] = x[i-1] + dt*k_prom_x;
+  y[i] = y[i-1] + dt*k_prom_y;
+  vx[i] = vx[i-1] + dt*k_prom_vx;
+  vy[i] = vy[i-1] + dt*k_prom_vy;
 
-
+  cout << t << " " << x << " " << y << " " << " " vx << " " << vy;
 
 }
-
-
 
 }
 
