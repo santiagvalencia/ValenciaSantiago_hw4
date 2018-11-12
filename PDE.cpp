49ba8b86ed7474f6370a9e5d59d367c013980202
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-11-12 00:58:04 -0500

    Constantes del problema

---
+++ PDE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <array>
+#include <string>
+
+using namespace std;
+
+#define PI 3.14159265359
+#define k 1.62
+#define Cp 820.0
+#define rho 2710
+#define nu k/(Cp*rho)
+
+int main(){
+
+}
