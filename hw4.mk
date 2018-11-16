8fc5a089435ceaf50730a4a05f13239918d61dcf
Santiago Valencia <s.valencia@uniandes.edu.co>
2018-11-15 23:06:53 -0500

    Nuevas variables de datos y gráficas. Incluye los resultados de PDEs en el pdf final.

--- hw4.mk
+++ hw4.mk
@@ -1,20 +1,28 @@
-Resultados_hw4.pdf: Tray_45.pdf Tray_varias.pdf Resultados_hw4.tex
+datosODE = RG_45.txt RG_10.txt RG_20.txt RG_30.txt RG_40.txt RG_50.txt RG_60.txt RG_70.txt
+graficasODE = Tray_45.pdf Tray_varias.pdf
+
+datosPDE = final_A.txt final_F.txt final_P.txt inicial.txt intermedio1_A.txt intermedio1_F.txt intermedio1_P.txt intermedio2_A.txt intermedio2_F.txt intermedio2_P.txt promedios.txt
+graficasPDE = final_A.pdf final_F.pdf final_P.pdf inicial_A.pdf inicial_F.pdf inicial_P.pdf intermedio1_A.pdf intermedio1_F.pdf intermedio1_P.pdf intermedio2_A.pdf intermedio2_F.pdf intermedio2_P.pdf promedios.pdf
+
+Resultados_hw4.pdf: $(graficasODE) $(graficasPDE) Resultados_hw4.tex
 	pdflatex Resultados_hw4.tex
 
-datos = RG_45.txt RG_10.txt RG_20.txt RG_30.txt RG_40.txt RG_50.txt RG_60.txt RG_70.txt
-
-Tray_45.pdf Tray_varias.pdf: $(datos)
+$(graficasODE) $(graficasPDE): $(datosODE) $(datosPDE)
 	python Plots_hw4.py
 
-$(datos): ODE.cpp
+$(datosODE): ODE.cpp
 	g++ ODE.cpp
+	./a.out
+
+$(datosPDE): PDE.cpp
+	g++ PDE.cpp
 	./a.out
 
 .PHONY : clean
 
 clean:
 	rm -r *.txt
-	rm -r *.log
 	rm -r *.pdf
-	rm -r *.aux
 	rm -r *.out
+	rm -r *.log
+	rm -r *.aux
